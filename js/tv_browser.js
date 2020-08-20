cd159f705145774339a37f373997b66c680e6950
Hilman Azfar <m.hilman.azfar@gmail.com>
2020-08-21 00:12:28 +0800

    Completed api request with async await

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,67 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+async function getData(query){
+    const url = `http://api.tvmaze.com/api/search/shows?q=${query}`;
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        addOptions(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+let handleSearchChange =(e)=>{
+    let query = e.target.value;
+    getData(query);
+    e.target.value = '';
+}
+
+let addOptions =(arr)=>{
+    let showSelect = document.getElementById("show-select");
+    arr.forEach((item)=>{
+        let option = document.createElement('option');
+        option.value = item.show.id;
+        option.innerText = item.show.name;
+        showSelect.appendChild(option);
+    })
+    showSelect.addEventListener('input', (e)=>{
+        getSingleSearch(e.target.value);
+    });
+}
+
+async function getSingleSearch(id){
+    const url = `http://api.tvmaze.com/shows/${id}`;
+    let showDetail = document.getElementById("show-detail");
+    try{
+        let response = await fetch(url);
+        let data = await response.json();
+
+        let heading = document.createElement('h2');
+        let image = document.createElement('img');
+        let para = document.createElement('p');
+
+        heading.innerText = data.name;
+        image.src = data.image.medium;
+        para.textContent = data.summary;
+
+        showDetail.appendChild(heading);
+        showDetail.appendChild(image);
+        showDetail.appendChild(para);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//
